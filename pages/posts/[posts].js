c12b7abfc2cd3afa27d0ece696a89ea1cb617470
kumarsuraj151 <102572879+kumarsuraj151@users.noreply.github.com>
2023-02-12 17:59:06 +0530

    made some changes

--- pages/posts/[posts].js
+++ pages/posts/[posts].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionItem,
@@ -11,11 +11,10 @@ import {
 import styles from "@/styles/Home.module.css";
 import { FaComment } from "react-icons/fa";
 import axios from "axios";
+import Comments from "@/components/Comments";
 
 export default function Posts({ data }) {
-const getComment=(id)=>{
-  console.log(id)
-}
+  
   return (
     <div className={styles.postMain}>
       <div className={styles.first_div_post}>
@@ -30,18 +29,12 @@ const getComment=(id)=>{
             <Box fontSize={'1.3em'} color={'black'} className={styles.para}  dangerouslySetInnerHTML={{ __html: data.body}}/>
           </Box>
         </h2>
-        <AccordionButton position={"relative"} paddingLeft="0px" onClick={()=>getComment(data.id)}>
+        <AccordionButton position={"relative"} paddingLeft="0px">
           <AccordionIcon />
           <FaComment />
          </AccordionButton>
         <AccordionPanel pb={4} paddingLeft="41px">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Lorem, ipsum dolor sit amet
-          consectetur adipisicing elit. Ea nam, architecto dignissimos,
-          sapiente ex unde suscipit deserunt laboriosam voluptatibus, debitis
-          nemo ipsum voluptates? Voluptatem in aperiam quasi esse tempora quas
+          <Comments id={data.id}/>
         </AccordionPanel>
       </AccordionItem>
     </Accordion>
