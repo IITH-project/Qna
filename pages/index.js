22b91214a90e9cab0d69bb0bcb4c099a20104d94
shivanshu <shivanshu8211@gmail.com>
2023-03-05 21:45:52 +0530

    Links added in front page

--- pages/index.js
+++ pages/index.js
@@ -17,38 +17,18 @@ const Home = (props) => {
           className={styles['wallpaperflare1']}
         />
         <div className='frame'>
-        <img
-          src="/img/Rectangle 1.png"
-          alt="Rectangle11549"
-          className={styles['rectangle1']}
-        />
-        <img
-          src="/img/Rectangle 3.png"
-          alt="Rectangle31550"
-          className={styles['rectangle3']}
-        />
-        <img
-          src="/img/Rectangle 4.png"
-          alt="Rectangle41551"
-          className={styles['rectangle4']}
-        />
-        <img
-          src="/img/Rectangle 2.png"
-          alt="Rectangle21552"
-          className={styles['rectangle2']}
-        />
-        <span className={styles['text']}>
-          <span>Home</span>
-        </span>
-        <span className={styles['text02']}>
-          <span>Blog</span>
-        </span>
-        <span className={styles['text04']}>
-          <span>Pages</span>
-        </span>
-        <span className={styles['text06']}>
-          <span>Contact</span>
-        </span>
+        <a href='http://localhost:3000/frontPage/1'>
+          <span className={styles['text']}>Home</span>
+        </a>
+        <a>
+        <span className={styles['text02']}>Blog</span>
+        </a>
+        <a>
+        <span className={styles['text04']}>About us</span>
+        </a>
+        <a>
+        <span className={styles['text06']}>Contact</span>
+        </a>
         </div>
         <div className={styles.Typing}>
           <h1 className={styles.develop}>Developers</h1>
--- pages/posts/[posts].js
+++ pages/posts/[posts].js
@@ -75,10 +75,6 @@ export default function Posts({ data1 }) {
         <div className={styles1.ele}>Comments: </div>
         <div className={styles1.ele}>Tags: </div>
         </div>
-        {/* <div className={styles1.answered}>
-        <div>Answred By:</div>
-        <div>Answred On:</div>
-        </div> */}
         </div>
     </div>
     {/* {richtext && <Box width={'60%'}><RichText id={router.query.posts} Alldata={data} setData={setdata} /></Box>} */}
